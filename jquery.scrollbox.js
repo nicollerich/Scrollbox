f981afba0af740afbfc5ec1700cabd054ad268b4
Ryan Barry <ryanc.barry@yahoo.com>
2013-02-26 19:51:22 -0800

    Beginning scrollToPage

--- jquery.scrollbox.js
+++ jquery.scrollbox.js
@@ -6,15 +6,16 @@
   $.fn.scrollbox = function(options) {
 
   	var settings = $.extend({
-  		pageIndicator:true
+  		pageIndicator:true,
+  		scrollToPage:true
     }, options);
 
     var lastPage = 0;
+    var contentWidth = 0;
 
     var methods = {
     	init: function() {
 	  		boxWidth = $(this).width();
-		  	contentWidth = 0;
 
 		    $(this).find('li').each(function(i, elem) {
 		    	var w = $(elem).width();
@@ -24,18 +25,23 @@
 		    numPages = methods.numPages.apply(this);
 
 		    if (settings.pageIndicator) {
-		    	$(this).after('<ul class="searchbox-page-indicator"></ul>');
+		    	$(this).after('<ul class="scrollbox-page-indicator"></ul>');
 
 		    	var page = methods.curPage.apply(this);
 		    	for(var i = 0; i < numPages; i++) {
 
 		    		var classStr = '';
-
-		    		if (i === page - 1) {
+		    		var current = (i === (page - 1));
+		    		if (current) {
 		    			classStr = ' class="current"';
 		    		}
+		    		
+		    		var li = '<li'+classStr+'></li>';
+		    		if (!current && settings.scrollToPage) {
+		    			li = '<a href="javascript:void(0)">'+li+'</a>';
+		    		}
 
-		    		$('.searchbox-page-indicator').append('<li'+classStr+'></li>');
+		    		$('.scrollbox-page-indicator').append(li);
 		    	}
 		    }
     	},
@@ -55,14 +61,26 @@
 			pageChanged: function(newPage) {
 				console.log('pageChanged to '+newPage);
 
-				$('.searchbox-page-indicator li.current').removeClass('current');
-				$('.searchbox-page-indicator li:nth-child('+newPage+')').addClass('current');
+				if (settings.pageIndicator) {
+					var $indicator = $('.scrollbox-page-indicator');
+
+					var $current = $indicator.find('li.current').removeClass('current');
+
+					if (settings.scrollToPage) {
+						$current.wrap('<a href="javascript:void(0)"></a>');
+						$indicator.find('a:nth-child('+newPage+') li').addClass('current').unwrap();
+					}
+					else {
+						$indicator.find('li:nth-child('+newPage+')').addClass('current');
+					}
+				}
 			},
 			offsetForPage: function(pageNum) {
 				return (pageNum - 1) * boxWidth;
 			},
 			scrollToPage: function(pageNum) {
-
+				var offset = methods.offsetForPage.apply(this);
+				// $('.')
 			}
     };
     m_ = methods;	// debug
